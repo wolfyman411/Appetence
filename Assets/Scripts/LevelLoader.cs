8eaff41b6c7343b7bb57e067e05accf1aed97e77
Paul Lian <49074561+vk3002h@users.noreply.github.com>
2024-08-01 14:59:29 +0800

    Modify

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -16,7 +16,6 @@ public class LevelLoader : MonoBehaviour
 
     public IEnumerator LoadLevel(string loadLevelName)
     {
-        Debug.Log("Trigger");
         animator.SetTrigger("Start");
         yield return new WaitForSeconds(animationTime);
         SceneManager.LoadScene(loadLevelName);
